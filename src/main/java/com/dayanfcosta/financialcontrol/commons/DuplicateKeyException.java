143dddc32265192be9c2d0a78f6c7db13d4aad93
Dayan Costa <dayan.costa@trustly.com>
2020-03-01 19:37:55 +0000

    Fixing javadoc

--- src/main/java/com/dayanfcosta/financialcontrol/commons/DuplicateKeyException.java
+++ src/main/java/com/dayanfcosta/financialcontrol/commons/DuplicateKeyException.java
@@ -4,9 +4,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.dao.DataIntegrityViolationException;
 
 /**
- * This annotation is responsible for customize message for {@link com.mongodb.DuplicateKeyException}
+ * This annotation is responsible for customize message for {@link DataIntegrityViolationException}
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
